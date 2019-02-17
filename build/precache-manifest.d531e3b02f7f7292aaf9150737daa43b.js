d938912991ee3f668647c548ef9b68cbd7d4e31c
Secret Keeper <secretkeeperapp@protonmail.com>
2019-02-17 10:40:30 -0800

    add first build snapshot

---
+++ build/precache-manifest.d531e3b02f7f7292aaf9150737daa43b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fc4522af73e5216ca756f353ce531d8",
+    "url": "/static/media/hardware_wallet_instructions.6fc4522a.md"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1100436fd972010c80de",
+    "url": "/static/js/main.1100436f.chunk.js"
+  },
+  {
+    "revision": "23b36421299d7410f275",
+    "url": "/static/js/1.23b36421.chunk.js"
+  },
+  {
+    "revision": "1100436fd972010c80de",
+    "url": "/static/css/main.1b52a740.chunk.css"
+  },
+  {
+    "revision": "23b36421299d7410f275",
+    "url": "/static/css/1.db9ba6a0.chunk.css"
+  },
+  {
+    "revision": "6ba5ffa7beca3209167c56a004763f38",
+    "url": "/index.html"
+  }
+];
